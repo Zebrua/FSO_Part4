bd3898929f50ca8d4bbc674f050657d6defde308
Zebrua <141924342+Zebrua@users.noreply.github.com>
2026-08-24 17:57:01 +0300

    Refactor test_helper.js with updated blog data

--- 4-8_4-14/test_helper.js
+++ 4-8_4-14/test_helper.js
@@ -1,25 +1,47 @@
-const Blog = require('../models/blog.js')
+const Blog = require('../models/blog')
+const {connect, disconnect} = require('../utils/middleware')
 
-initialList = [
+const initial_blogs = [
     {
-      "title": "NewTitle",
-      "author": "ME",
-      "url": "http:/bussdownforwhat.con",
-      "likes": 3,
-      "id": "66c5eadeb3a72e48fb2eef91"
+        "title": "Pepper Sidcom",
+        "author": "Nevel Recon",
+        "url": "http://Peppersidc.con",
+        "likes": 2,
+        "id": "66c8ae86faa26eb9639a5089"
     },
     {
-      "title": "Pepper Sidcom",
-      "author": "Nevel Recon",
-      "url": "http://Peppersidc.con",
-      "likes": 1,
-      "id": "66c5eadeb3a72e48fb2eef84"
+        "title": "GOGO_Blogging",
+        "author": "Penol Damase",
+        "url": "https://newBlogpage.con",
+        "likes": 1,
+        "id": "6a885e03412c5541469164dc"
+    },
+    {
+        "title": "New Blog",
+        "author": "Thomas Hudson",
+        "url": "https://newwave.con",
+        "likes": 0,
+        "id": "6a885f69cd97215fb8ba6908"
     }
 ]
 
-const DB_List = async () => {
-    const BlogPosts = await Blog.find({})
-    return BlogPosts.map(blog => blog.toJSON())
+const nonExistingId = async () => {
+  await connect()
+  const blog = new Blog({ "title": 'new' })
+  await blog.save()
+  await blog.deleteOne()
+  await disconnect()
+  return blog._id.toString()
 }
 
-module.exports = { initialList, DB_List }
+
+const ArtifactsInDb = async () => {
+  await connect()
+  const blogs = await Blog.find({})
+  await disconnect()
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initial_blogs, nonExistingId, ArtifactsInDb
+}
